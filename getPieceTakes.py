62a10bcf61ef5748ccaf1fa62c0ed1cca20a84fb
Bryan <franciscobryaaan@gmail.com>
2024-07-19 14:50:47 +0800

    filter functionality working on piece captures

--- getPieceTakes.py
+++ getPieceTakes.py
@@ -8,14 +8,21 @@ def getPieceTakesData(ratingFilter1, ratingFilter2, timeControl, openingFilter):
         cumu_elo_takes = json.load(jsonfile)
 
     ratingFilter1 -= 1
-
     r1, r2 = str(ratingFilter1), str(ratingFilter2)
+
+    filename_r1 = "Cleaning/Cumulative/" + str(r1) + ".json"
+    filename_r2 = "Cleaning/Cumulative/" + str(r2) + ".json"
+
+    with open(filename_r1, 'r') as jsonfile:
+        left_cumu = json.load(jsonfile)
+    with open(filename_r2, 'r') as jsonfile:
+        right_cumu = json.load(jsonfile)
 
     # ['Pawn', 'Rook', 'Knight', 'Bishop', 'Queen', 'King']
     pieces = "PRNBQK"
     takes_in_range = []
 
     for p in pieces:
-        takes_in_range.append(cumu_elo_takes[r2][p] - cumu_elo_takes[r1][p])
+        takes_in_range.append(right_cumu[timeControl][openingFilter][p] - left_cumu[timeControl][openingFilter][p])
     
     return takes_in_range
