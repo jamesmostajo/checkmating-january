adf0563191d7c2ef18d1a828d1840404c1d9ea56
Bryan <franciscobryaaan@gmail.com>
2024-07-19 14:21:53 +0800

    updated ExtractResult.py

--- Cleaning/ExtractResult.py
+++ Cleaning/ExtractResult.py
@@ -2,12 +2,14 @@ import csv, json
 from Openings import openings
 
 results = ["Draw", "Checkmate", "Abandoned", "Resigned", "Time Forfeit"]
+pieceCaptures = list("PRNBQK")
+innerDimension = results + pieceCaptures
 gameTypes = ["All", "Blitz", "Rapid", "Classical"]
 
 EloTimeResOpenings = {
     elo: {
         gameType: {
-            opening: {res: 0 for res in results}
+            opening: {res: 0 for res in innerDimension}
             for opening in openings
         }
         for gameType in gameTypes
@@ -18,7 +20,7 @@ EloTimeResOpenings = {
 CumuEloTimeResOpenings = {
     elo: {
         gameType: {
-            opening: {res: 0 for res in results}
+            opening: {res: 0 for res in innerDimension}
             for opening in openings
         }
         for gameType in gameTypes
@@ -26,7 +28,7 @@ CumuEloTimeResOpenings = {
     for elo in range(2299, 3001)
 }
 
-filesource = "Cleaning/info_data.csv"
+filesource = "Cleaning/info_data_withmoves.csv"
 elofile = "Cleaning/EloInfo.csv"
 game_elo = [] 
 
@@ -42,7 +44,7 @@ with open(filesource, 'r') as file:
     filereader = csv.reader(file)
 
     for row in filereader:
-        game_id, whiteElo, blackElo, timeControl, lastMove, termination, result, opening = row
+        game_id, whiteElo, blackElo, timeControl, lastMove, termination, result, opening, movesRaw = row
         game_id = int(game_id)
         score = result.split('-')
         cur_elo = game_elo[game_id]
@@ -59,16 +61,31 @@ with open(filesource, 'r') as file:
             result_type = "Time Forfeit"
         else:
             continue
+        
+        moves = movesRaw.split(';')
 
         EloTimeResOpenings[cur_elo][timeControl][opening][result_type] += 1
         EloTimeResOpenings[cur_elo][timeControl]["All"][result_type] += 1
         EloTimeResOpenings[cur_elo]["All"][opening][result_type] += 1
         EloTimeResOpenings[cur_elo]["All"]["All"][result_type] += 1
 
+        for move in moves:
+            if 'x' in move:
+                capturing_piece = move[move.index('x') - 1]
+                if capturing_piece not in "RNBQK":
+                    capturing_piece = "P"
+                
+                EloTimeResOpenings[cur_elo][timeControl][opening][capturing_piece] += 1
+                EloTimeResOpenings[cur_elo][timeControl]["All"][capturing_piece] += 1
+                EloTimeResOpenings[cur_elo]["All"][opening][capturing_piece] += 1
+                EloTimeResOpenings[cur_elo]["All"]["All"][capturing_piece] += 1
+
+
+
 for elo in range(2300, 3001):
     for gameType in gameTypes:
         for opening in openings:
-            for result in results:
+            for result in innerDimension:
                 CumuEloTimeResOpenings[elo][gameType][opening][result] = (
                     EloTimeResOpenings[elo][gameType][opening][result] +
                     CumuEloTimeResOpenings[elo-1][gameType][opening][result]
